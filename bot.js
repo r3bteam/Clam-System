a904d788c3a38d38e8a72933219d9e33d47d742e
r3bteam <39262475+r3bteam@users.noreply.github.com>
2018-10-15 23:55:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -194,5 +194,42 @@ client.on('message', async message => {
        
     }
 })
+client.on('message', message => {
+  var prefix = "+";
+  const guild = message.guild;
+
+  if (message.author.codes) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "ban") {
+               if(!message.channel.guild) return message.reply('** This command only for servers**');
+         
+  if(!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) return message.reply("**انت لا تملك الصلاحيات المطلوبه**");
+  if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) return message.reply("**I Don't Have ` BAN_MEMBERS ` Permission**");
+  let user = message.mentions.users.first();
+  let reason = message.content.split(" ").slice(2).join(" ");
+
+  if (message.mentions.users.size < 1) return message.reply("**منشن شخص**");
+  if(!reason) return message.reply ("**اكتب سبب الطرد**");
+  if (!message.guild.member(user)
+  .bannable) return message.reply("**يجب ان تكون رتبة البوت اعلي من رتبه الشخص المراد تبنيدة**");
+
+
+  message.guild.member(user).ban(7, user);
+
+message.channel.send(`**:white_check_mark: ${user.tag} Banned From The Server By : <@${message.author.id}> Reason: ${reason} ! :airplane: **  `)
+  
+guild.owner.send(`سيرفر : ${guild.name}
+**تم تبنيد** :${user.tag}  
+**بواسطة** : <@${message.author.id}>
+**السبب** : ${reason} `)
+
+}
+});
 
             client.login(process.env.BOT_TOKEN);
