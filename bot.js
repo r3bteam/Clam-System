2007f6646baab2fec3becd0227cde7898a7b2578
r3bteam <39262475+r3bteam@users.noreply.github.com>
2018-12-04 20:39:16 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,128 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const RichEmbed = require("discord.js");
 const { Client, Util } = require('discord.js');
+const fs = require('fs');
+const data = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
+ 
+client.on('guildMemberRemove', (u) => {
+    u.guild.fetchAuditLogs().then( s => {
+        var ss = s.entries.first();
+        if (ss.action == "MEMBER_KICK") {
+        if (!data[ss.executor.id]) {
+            data[ss.executor.id] = { 
+            time : 1
+          };
+      } else { 
+          data[ss.executor.id].time+=1
+      }; 
+data[ss.executor.id].time = 0
+u.guild.roles.forEach(r => {
+	r.edit({
+                    permissions : 104160320
+                }); 
+                data[ss.executor.id].time = 0
+            });
+        setTimeout(function(){
+            if (data[ss.executor.id].time <= 3) { 
+                data[ss.executor.id].time = 0
+            }
+        },60000)
+    };
+    });
+    fs.writeFile("./data.json", JSON.stringify(data) ,(err) =>{
+        if (err) console.log(err.message);
+    });
+});
+client.on('roleDelete', (u) => { 
+    guild = u.guild;
+    roles = u.guild;
 
+    u.guild.fetchAuditLogs().then( s => {
+        var ss = s.entries.first();
+        if (ss.action == "ROLE_DELETE") {
+        if (!data[ss.executor.id]) {
+            data[ss.executor.id] = {
+            time : 1
+          };
+      } else {
+          data[ss.executor.id].time+=1
+      };
+data[ss.executor.id].time = 0
+u.guild.roles.forEach(r => {
+	r.edit({
+                    permissions : 104160320
+                }); 
+                data[ss.executor.id].time = 0
+            });
+        setTimeout(function(){
+            if (data[ss.executor.id].time <= 3) {
+                data[ss.executor.id].time = 0
+            }
+        },60000)
+    };
+    });
+    fs.writeFile("./data.json", JSON.stringify(data) ,(err) =>{
+        if (err) console.log(err.message);
+    });
+});
+client.on('channelDelete', (u) => {
+    u.guild.fetchAuditLogs().then( s => { 
+        var ss = s.entries.first();
+        if (ss.action == "CHANNEL_DELETE") {
+        if (!data[ss.executor.id]) {
+            data[ss.executor.id] = {
+            time : 1
+          };
+      } else {
+          data[ss.executor.id].time+=1 
+      };
+data[ss.executor.id].time = 0
+u.guild.roles.forEach(r => {
+	r.edit({
+                    permissions : 104160320
+                }); 
+                data[ss.executor.id].time = 0
+            });
+        setTimeout(function(){
+            if (data[ss.executor.id].time <= 3) {
+                data[ss.executor.id].time = 0
+            }
+        },60000)
+    };
+    });
+    fs.writeFile("./data.json", JSON.stringify(data) ,(err) =>{
+        if (err) console.log(err.message);
+    });
+});
+client.on('guildBanAdd', (g , u) => {
+    g.fetchAuditLogs().then( s => {
+        var ss = s.entries.first();
+        if (ss.action == `MEMBER_BAN_ADD`) {
+        if (!data[ss.executor.id]) {
+            data[ss.executor.id] = {
+            time : 1
+          };
+      } else {
+          data[ss.executor.id].time+=1
+      };
+        if (data[ss.executor.id].time >= 3) {
+            g.roles.forEach(r => {
+                r.edit({ 
+                    permissions : 104160320
+                });  
+            }); 
+        }
+        setTimeout(function(){
+            if (data[ss.executor.id].time <= 3) {  
+                data[ss.executor.id].time = 0
+            } 
+        },60000)
+    }; 
+    });      
+    fs.writeFile("./data.json", JSON.stringify(data) ,(err) =>{    
+        if (err) console.log(err.message);   
+    });     
+});    
 client.on('message', message => {
     if(!message.channel.guild) return;
 let args = message.content.split(' ').slice(1).join(' ');
