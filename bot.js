292ac442024d9a6d584b12adc663b81cadd5acfc
r3bteam <39262475+r3bteam@users.noreply.github.com>
2019-01-23 23:50:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,127 +3,105 @@ const client = new Discord.Client();
 const RichEmbed = require("discord.js");
 const { Client, Util } = require('discord.js');
 const fs = require('fs');
-const data = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
- 
-client.on('guildMemberRemove', (u) => {
-    u.guild.fetchAuditLogs().then( s => {
-        var ss = s.entries.first();
-        if (ss.action == "MEMBER_KICK") {
-        if (!data[ss.executor.id]) {
-            data[ss.executor.id] = { 
-            time : 1
-          };
-      } else { 
-          data[ss.executor.id].time+=1
-      }; 
-data[ss.executor.id].time = 0
-u.guild.roles.forEach(r => {
-	r.edit({
-                    permissions : 104160320
-                }); 
-                data[ss.executor.id].time = 0
-            });
-        setTimeout(function(){
-            if (data[ss.executor.id].time <= 3) { 
-                data[ss.executor.id].time = 0
-            }
-        },60000)
-    };
-    });
-    fs.writeFile("./data.json", JSON.stringify(data) ,(err) =>{
-        if (err) console.log(err.message);
-    });
-});
-client.on('roleDelete', (u) => { 
-    guild = u.guild;
-    roles = u.guild;
+const antic = JSON.parse(fs.readFileSync('./antic.json', 'utf8'));
+client.on("message", message =>{
+if(!antic[message.author.id]) {
+antic[message.author.id] = {
+actions: 0
+}}
+})
 
-    u.guild.fetchAuditLogs().then( s => {
-        var ss = s.entries.first();
-        if (ss.action == "ROLE_DELETE") {
-        if (!data[ss.executor.id]) {
-            data[ss.executor.id] = {
-            time : 1
-          };
-      } else {
-          data[ss.executor.id].time+=1
-      };
-data[ss.executor.id].time = 0
-u.guild.roles.forEach(r => {
-	r.edit({
-                    permissions : 104160320
-                }); 
-                data[ss.executor.id].time = 0
-            });
-        setTimeout(function(){
-            if (data[ss.executor.id].time <= 3) {
-                data[ss.executor.id].time = 0
-            }
-        },60000)
-    };
+
+client.on('guildMemberRemove', alpha => {
+alpha.guild.fetchAuditLogs().then( ac => {
+var anti = ac.entries.first();
+if(anti.action == "MEMBER_KICK") {
+if(!antic[anti.executor.id]) {
+antic[anti.executor.id] = {
+actions: 0
+};
+} else { 
+antic[anti.executor.id].actions+=1
+if (antic[anti.executor.id].actions == 5) {
+alpha.guild.members.get(anti.executor.id).ban("AntiHack")
+console.log("banned griefer 1")
+antic[anti.executor.id].actions = 0
+}
+}
+    }
     });
-    fs.writeFile("./data.json", JSON.stringify(data) ,(err) =>{
+    fs.writeFile("./antic.json", JSON.stringify(antic) ,(err) =>{
         if (err) console.log(err.message);
     });
 });
-client.on('channelDelete', (u) => {
-    u.guild.fetchAuditLogs().then( s => { 
-        var ss = s.entries.first();
-        if (ss.action == "CHANNEL_DELETE") {
-        if (!data[ss.executor.id]) {
-            data[ss.executor.id] = {
-            time : 1
-          };
-      } else {
-          data[ss.executor.id].time+=1 
-      };
-data[ss.executor.id].time = 0
-u.guild.roles.forEach(r => {
-	r.edit({
-                    permissions : 104160320
-                }); 
-                data[ss.executor.id].time = 0
-            });
-        setTimeout(function(){
-            if (data[ss.executor.id].time <= 3) {
-                data[ss.executor.id].time = 0
-            }
-        },60000)
-    };
+client.on('roleDelete', alpha => {
+alpha.guild.fetchAuditLogs().then( ac => {
+var anti = ac.entries.first();
+if(anti.action == "ROLE_DELETE") {
+if(!antic[anti.executor.id]) {
+antic[anti.executor.id] = {
+actions: 0
+};
+} else { 
+antic[anti.executor.id].actions+=1
+if (antic[anti.executor.id].actions == 3) {
+alpha.guild.members.get(anti.executor.id).ban("AntiHack")
+console.log("banned griefer 1")
+antic[anti.executor.id].actions = 0
+}
+}
+    }
     });
-    fs.writeFile("./data.json", JSON.stringify(data) ,(err) =>{
+    fs.writeFile("./antic.json", JSON.stringify(antic) ,(err) =>{
         if (err) console.log(err.message);
     });
 });
-client.on('guildBanAdd', (g , u) => {
-    g.fetchAuditLogs().then( s => {
-        var ss = s.entries.first();
-        if (ss.action == `MEMBER_BAN_ADD`) {
-        if (!data[ss.executor.id]) {
-            data[ss.executor.id] = {
-            time : 1
-          };
-      } else {
-          data[ss.executor.id].time+=1
-      };
-        if (data[ss.executor.id].time >= 3) {
-            g.roles.forEach(r => {
-                r.edit({ 
-                    permissions : 104160320
-                });  
-            }); 
-        }
-        setTimeout(function(){
-            if (data[ss.executor.id].time <= 3) {  
-                data[ss.executor.id].time = 0
-            } 
-        },60000)
-    }; 
-    });      
-    fs.writeFile("./data.json", JSON.stringify(data) ,(err) =>{    
-        if (err) console.log(err.message);   
-    });     
-});    
+client.on('channelDelete', alpha => {
+alpha.guild.fetchAuditLogs().then( ac => {
+var anti = ac.entries.first();
+if(anti.action == "CHANNEL_DELETE") {
+if(!antic[anti.executor.id]) {
+antic[anti.executor.id] = {
+actions: 0
+};
+} else { 
+antic[anti.executor.id].actions+=1
+if (antic[anti.executor.id].actions == 1) {
+alpha.guild.members.get(anti.executor.id).ban("AntiHack")
+console.log("banned griefer 1")
+antic[anti.executor.id].actions = 0
+}
+}
+    }
+    });
+    fs.writeFile("./antic.json", JSON.stringify(antic) ,(err) =>{
+        if (err) console.log(err.message);
+    });
+});
+
+client.on('guildBanAdd', function(alpha){
+alpha.fetchAuditLogs().then( ac => {
+var anti = ac.entries.first();
+if(anti.action == "MEMBER_BAN_ADD") {
+if(!antic[anti.executor.id]) {
+antic[anti.executor.id] = {
+actions: 0
+};
+} else { 
+antic[anti.executor.id].actions+=1
+if (antic[anti.executor.id].actions == 3) {
+alpha.members.get(anti.executor.id).ban("AntiHack")
+console.log("banned griefer 1")
+antic[anti.executor.id].actions = 0
+}
+}
+    }
+    });
+    fs.writeFile("./antic.json", JSON.stringify(antic) ,(err) =>{
+        if (err) console.log(err.message);
+    });
+});
+ 
 client.on('message', message => {
     if(!message.channel.guild) return;
 let args = message.content.split(' ').slice(1).join(' ');
